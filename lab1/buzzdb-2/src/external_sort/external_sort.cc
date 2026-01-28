d2e15551d64fe905b8eea0857c0cf2106b0bc634
Shivam Singhal <shivamsinghal16032003@gmail.com>
2026-01-27 21:08:55 -0500

    initial working implementation of lab1

--- lab1/buzzdb-2/src/external_sort/external_sort.cc
+++ lab1/buzzdb-2/src/external_sort/external_sort.cc
@@ -14,24 +14,11 @@
 
 #define UNUSED(p) ((void)(p))
 #define CEIL(a, b) (a-1)/b + 1
+#define LONGSIZE 8
 
 namespace buzzdb {
 
 using heapPair = std::pair<uint64_t, std::pair<uint64_t, uint64_t>>;
-
-
-constexpr size_t WORD_SIZE = sizeof(uint64_t);
-
-struct MergeNode {
-    uint64_t value;
-    uint64_t bucket_idx;
-    uint64_t element_idx;
-    
-    bool operator>(const MergeNode& other) {
-        return value > other.value;
-    }
-};
-
 
 class KMergeExternalSort {
     private:
@@ -39,59 +26,55 @@ class KMergeExternalSort {
     uint64_t maxNumsPerBucket, totalBuckets, numsPerPartialBuffer;
     std::vector<std::unique_ptr<File>> temp_files;
     uint64_t num_values, mem_size;
-    std::vector<std::vector<uint64_t>> bucketPartialBuffers;
+    std::vector<uint64_t*> bucketPartialBuffers;
 
     void readNextElements(uint64_t bucketIndex, uint64_t offset) {
-        uint64_t numElements = std::min(numsPerPartialBuffer, temp_files[bucketIndex]->size() / WORD_SIZE - offset);
-        auto& buffer = bucketPartialBuffers[bucketIndex];
-        buffer.resize(numElements);
-        temp_files[bucketIndex]->read_block(offset*WORD_SIZE, numElements * WORD_SIZE, reinterpret_cast<char*>(buffer.data()));
+        uint64_t numElements = std::min(numsPerPartialBuffer, temp_files[bucketIndex]->size() / LONGSIZE - offset);
+        temp_files[bucketIndex]->read_block(offset*LONGSIZE, numElements * LONGSIZE, (char*)bucketPartialBuffers[bucketIndex]);
     }
 
     void partial_sort(File &input) {
-        std::vector<uint64_t> tmp_array(maxNumsPerBucket);
+        uint64_t* tmp_array = (uint64_t*)malloc(mem_size);
     
         for(uint64_t i = 0; i < totalBuckets; i++) {
             int bucketSize = std::min(maxNumsPerBucket, num_values - i*maxNumsPerBucket);
-            input.read_block(i*mem_size, bucketSize*WORD_SIZE, reinterpret_cast<char*>(tmp_array.data()));
-            std::sort(tmp_array.begin(), tmp_array.begin() + bucketSize);
+            input.read_block(i*mem_size, bucketSize*LONGSIZE, (char*)tmp_array);
+            std::sort(tmp_array, tmp_array + bucketSize);
             temp_files[i] = File::make_temporary_file();
-            temp_files[i]->resize(bucketSize*WORD_SIZE);
-            temp_files[i]->write_block(reinterpret_cast<char*>(tmp_array.data()), 0, bucketSize*WORD_SIZE);
+            temp_files[i]->resize(bucketSize*LONGSIZE);
+            temp_files[i]->write_block((char*) tmp_array, 0, bucketSize*LONGSIZE);
         }
+        free(tmp_array);
     }
 
     void kMerge(File &output) {
 
-        std::vector<uint64_t> headBucket(totalBuckets, 0);
-        std::priority_queue<MergeNode, std::vector<MergeNode>, std::greater<MergeNode>> sortedHeap;
-        
+        uint64_t headBucket[totalBuckets] = {0};
+        std::priority_queue<heapPair, std::vector<heapPair>, std::greater<heapPair>> sortedHeap;
         for(uint64_t bucketIndex = 0; bucketIndex < totalBuckets; bucketIndex++) {
+            bucketPartialBuffers[bucketIndex] = (uint64_t*)malloc(numsPerPartialBuffer * LONGSIZE);
             readNextElements(bucketIndex, headBucket[bucketIndex]);
-            if(!bucketPartialBuffers[bucketIndex].empty()) {
-                sortedHeap.push({bucketPartialBuffers[bucketIndex][0], bucketIndex, 0});
-            }
+            sortedHeap.push({bucketPartialBuffers[bucketIndex][0], {0, bucketIndex}});
         }
-
-        std::vector<uint64_t> sortedElements(numsPerPartialBuffer);
+        uint64_t* sortedElements = (uint64_t*)malloc(numsPerPartialBuffer * LONGSIZE);
 
         uint64_t bufferIndex = 0, totalNumbersWritten = 0;
-        while(!sortedElements.empty()) {
-
-            auto [val, bucketIndex, elementIndex] = sortedHeap.top();
+        while(true) {
+            if(sortedHeap.empty()) break;
+            heapPair topNode = sortedHeap.top();
             sortedHeap.pop();
-            sortedElements[bufferIndex] = val;
+            sortedElements[bufferIndex] = topNode.first;
             bufferIndex++;
 
             if(bufferIndex == numsPerPartialBuffer) {
-                output.write_block(reinterpret_cast<char*>(sortedElements.data()), totalNumbersWritten * WORD_SIZE, numsPerPartialBuffer*WORD_SIZE);
+                output.write_block((char*)sortedElements, totalNumbersWritten * LONGSIZE, numsPerPartialBuffer*LONGSIZE);
                 totalNumbersWritten += bufferIndex; 
                 bufferIndex = 0;
             }
-            uint64_t bucketIndex = topNode.bucket_idx;
-            std::vector<uint64_t>& bucketBuffer = bucketPartialBuffers[bucketIndex];
+            uint64_t bucketIndex = topNode.second.second;
+            uint64_t *bucketBuffer = bucketPartialBuffers[bucketIndex];
             headBucket[bucketIndex]++;
-            if(headBucket[bucketIndex] == temp_files[bucketIndex]->size() / WORD_SIZE) {
+            if(headBucket[bucketIndex] == temp_files[bucketIndex]->size() / LONGSIZE) {
                 continue;
             }
             if(headBucket[bucketIndex] % numsPerPartialBuffer == 0) {
@@ -103,7 +86,7 @@ class KMergeExternalSort {
             }
         }
         if(bufferIndex) {
-            output.write_block((char*)sortedElements, totalNumbersWritten * WORD_SIZE, bufferIndex*WORD_SIZE);
+            output.write_block((char*)sortedElements, totalNumbersWritten * LONGSIZE, bufferIndex*LONGSIZE);
         }
 
         for(uint64_t bucketIndex = 0; bucketIndex < totalBuckets; bucketIndex++) {
@@ -117,16 +100,16 @@ class KMergeExternalSort {
     KMergeExternalSort(size_t numValues, size_t memSize)
     :num_values(numValues), mem_size(memSize) {
 
-        maxNumsPerBucket = mem_size/WORD_SIZE;
+        maxNumsPerBucket = mem_size/LONGSIZE;
         totalBuckets = CEIL(num_values, maxNumsPerBucket);
         temp_files = std::vector<std::unique_ptr<File>>(totalBuckets);
-        numsPerPartialBuffer = (mem_size / (totalBuckets + 1)) / WORD_SIZE;
+        numsPerPartialBuffer = (mem_size / (totalBuckets + 1)) / LONGSIZE;
         bucketPartialBuffers = std::vector<uint64_t*>(totalBuckets);
     }
 
     void external_sort(File &input, File &output) {
         if(num_values == 0) return;
-        output.resize(num_values*WORD_SIZE);
+        output.resize(num_values*LONGSIZE);
         partial_sort(input);
         kMerge(output);
     }
